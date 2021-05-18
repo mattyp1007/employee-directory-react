6a1315aed73c71e6fd06c99dada03f5df78caee7
Matthew Parker <matt.parker1007@gmail.com>
2021-05-18 18:51:25 -0500

    map employee data to a table row

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -9,7 +9,7 @@ class EmployeeTable extends Component {
   componentDidMount = () => {
     axios.get('https://randomuser.me/api/?inc=name,email,dob,phone,picture&results=50')
       .then(function (response) {
-        // handle success
+        
         console.log(response);
       })
       .catch(function (error) {
@@ -21,7 +21,16 @@ class EmployeeTable extends Component {
   render() {
     return (
       <div>
-        
+        {this.state.employees.map(employee => {
+          <TableRow
+            name={employee.name}
+            email={employee.email}
+            dob={employee.dob}
+            phone={employee.phone}
+            picture={employee.picture}
+          />
+        })}
+
         <pre>Check console </pre>
       </div>
       
